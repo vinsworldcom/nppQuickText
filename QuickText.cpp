aaee8d07e1224516e0ea143aa7cbc5d037f0c953
VinsWorldcom <vin@vinsworld.com>
2021-11-03 09:45:08 -0400

    attempt to make XPM transparent with color white=>None (Scintilla Docs)

--- QuickText.cpp
+++ QuickText.cpp
@@ -65,7 +65,7 @@ const char *xpmQt[] = {
 /* columns rows colors chars-per-pixel */
     "16 16 2 1 ",
     "  c #010101",
-    ". c white",
+    ". c None",
     /* pixels */
     "................",
     ".... ...........",
